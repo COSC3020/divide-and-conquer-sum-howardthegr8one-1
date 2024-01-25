17bb79e5ef0a901375f5c2a57c63b0029fce3881
Howard Shaw <68131536+howardthegr8one@users.noreply.github.com>
2024-01-25 15:01:42 -0700

    Update code.js
    added my code

--- code.js
+++ code.js
@@ -1,3 +1,15 @@
 function divideAndConquerSum(a) {
-    return a;
+    let sum = 0;
+    if (a.length <= 2)
+        if (a.length <= 1)
+            return (a.length == 1) ? a[0] : 0;
+        else
+            return a[0] + a[1];
+    else {
+        let subsize = Math.floor(a.length / 3);
+        sum = divideAndConquerSum(a.slice(0, subsize)) + 
+              divideAndConquerSum(a.slice(subsize, 2*subsize)) + 
+              divideAndConquerSum(a.slice(2*subsize, a.length));
+    }
+    return sum;
 }
